574fb4d88e9f7bdcfe102ea7991d632a708006d7
shadekiller666 <ezioauditorerevs@gmail.com>
2021-08-18 11:01:12 -0700

    Adjusted piece images to line up with each other.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { preserveWhitespacesDefault } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
 import { Color, PieceComponent, Type } from '../piece/piece.component';
 import { Space, SpaceComponent } from '../space/space.component';
 
@@ -10,8 +11,11 @@ import { Space, SpaceComponent } from '../space/space.component';
 export class BoardComponent implements OnInit {
   brown: boolean = false;
   public spaces = new Array();
+  playerIsWhite: boolean = true;
 
   constructor() {
+    let playerColor = this.playerIsWhite ? Color.white : Color.black;
+    let cpuColor = this.playerIsWhite ? Color.black : Color.white;
     for (let col = 0; col < 8; col++) {
       this.spaces[col] = new Array();
       for (let row = 0; row < 8; row++) {
@@ -20,35 +24,35 @@ export class BoardComponent implements OnInit {
         switch(col) {
           case 0:
           case 7:
-            if (row == 0) this.spaces[col][row].setPiece(Type.rook, Color.white);
-            if (row == 7) this.spaces[col][row].setPiece(Type.rook, Color.black);
+            if (row == 0) this.spaces[col][row].setPiece(Type.rook, cpuColor);
+            if (row == 7) this.spaces[col][row].setPiece(Type.rook, playerColor);
             break;
           case 1:
           case 6:
-            if (row == 0) this.spaces[col][row].setPiece(Type.knight, Color.white);
-            if (row == 7) this.spaces[col][row].setPiece(Type.knight, Color.black);
+            if (row == 0) this.spaces[col][row].setPiece(Type.knight, cpuColor);
+            if (row == 7) this.spaces[col][row].setPiece(Type.knight, playerColor);
             break;
           case 2:
           case 5:
-            if (row == 0) this.spaces[col][row].setPiece(Type.bishop, Color.white);
-            if (row == 7) this.spaces[col][row].setPiece(Type.bishop, Color.black);
+            if (row == 0) this.spaces[col][row].setPiece(Type.bishop, cpuColor);
+            if (row == 7) this.spaces[col][row].setPiece(Type.bishop, playerColor);
             break;
           case 3:
-            if (row == 0) this.spaces[col][row].setPiece(Type.king, Color.white);
-            if (row == 7) this.spaces[col][row].setPiece(Type.king, Color.black);
+            if (row == 0) this.spaces[col][row].setPiece(Type.king, cpuColor);
+            if (row == 7) this.spaces[col][row].setPiece(Type.king, playerColor);
             break;
           case 4:
-            if (row == 0) this.spaces[col][row].setPiece(Type.queen, Color.white);
-            if (row == 7) this.spaces[col][row].setPiece(Type.queen, Color.black);
+            if (row == 0) this.spaces[col][row].setPiece(Type.queen, cpuColor);
+            if (row == 7) this.spaces[col][row].setPiece(Type.queen, playerColor);
             break;  
         }
 
         if (row == 1) {
-          this.spaces[col][row].setPiece(Type.pawn, Color.white);
+          this.spaces[col][row].setPiece(Type.pawn, cpuColor);
         }
 
         if (row == 6) {
-          this.spaces[col][row].setPiece(Type.pawn, Color.black);
+          this.spaces[col][row].setPiece(Type.pawn, playerColor);
         }
       }
     }
